6f9a8394fb1c155aebb35dedcaf71fc87827c25f
Fabio Maia <fabiomaia@ciandt.com>
2023-04-26 12:40:04 -0300

    [test]tests block region

--- examples/web-app-protection-example/environments/dev/main.tf
+++ examples/web-app-protection-example/environments/dev/main.tf
@@ -327,14 +327,15 @@ resource "google_compute_security_policy" "policy" {
   }
 
   rule {
-    action        = "allow"
-    priority      = "2147483647"
-    description   = "default rule"
+    action   = "allow"
+    priority = "2147483647"
     match {
-      expr {
-        expression    = ["*"]
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
       }
     }
+    description = "default rule"
   }
 }
 
