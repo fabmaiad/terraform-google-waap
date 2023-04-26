0c82b6ac5ccffb3995535672860bd2318a82c40e
Fabio Maia <fabiomaia@ciandt.com>
2023-04-26 15:44:12 -0300

    [test]tests block region

--- examples/web-app-protection-example/environments/dev/main.tf
+++ examples/web-app-protection-example/environments/dev/main.tf
@@ -348,7 +348,7 @@ module "lb-http" {
   source  = "GoogleCloudPlatform/lb-http/google"
   version = "7.0.0"
 
-  name        = "lb-web-app"
+  name        = "lb-web-app-1"
   project     = var.project_id
   target_tags = ["backend-r1", "backend-r2"]
 
