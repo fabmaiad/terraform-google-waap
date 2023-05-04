26424c2fc344aaa1474677e15cc85470f6139939
Matheus Buitrago <mbuitrago@ciandt.com>
2023-05-04 16:50:12 -0300

    fix: removed firewall rule that enables iap access

--- modules/mig_network/main.tf
+++ modules/mig_network/main.tf
@@ -48,22 +48,3 @@ module "cloud-nat" {
   router        = format("router-%s", var.network_name)
   name          = format("nat-%s", var.network_name)
 }
-
-/*********************************************************************************
-**** Firewall rule to allow incoming ssh connections from Google IAP servers. ****
-**********************************************************************************/
-resource "google_compute_firewall" "inbound-ip-ssh" {
-  name    = format("allow-ssh-iap-%s", var.network_name)
-  project = var.project_id
-  network = module.mig_vpc.network_name #!TODO
-
-  direction = "INGRESS"
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "8080"] # SSH port and Jenkins port
-  }
-  source_ranges = [
-    "35.235.240.0/20"
-  ]
-  source_tags = ["allow-ssh-iap"]
-}
