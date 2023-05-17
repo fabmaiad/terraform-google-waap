ee33f1a70cb95d0ec3cb147c3d78baaddd1f2a0c
Matheus Buitrago <mbuitrago@ciandt.com>
2023-05-17 09:54:25 -0300

    [feature] organized folders for multiple environments

---
+++ examples/web-app-protection-example/scripts/startup-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+# Install docker and run the juice shop application.
+sudo apt-get -y install ca-certificates curl gnupg lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker pull bkimminich/juice-shop
+docker run -d -p 80:3000 bkimminich/juice-shop
