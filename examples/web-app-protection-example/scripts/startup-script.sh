e5564bad086a17c156087a7d2ace8fc086c008de
Matheus Buitrago <mbuitrago@ciandt.com>
2023-05-24 10:35:13 -0300

    enabled juice-shop app

--- examples/web-app-protection-example/scripts/startup-script.sh
+++ examples/web-app-protection-example/scripts/startup-script.sh
@@ -2,7 +2,7 @@
 set -x
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-# Install docker and run the juice shop application.
+## Install docker and run the juice shop application.
 sudo apt-get -y install ca-certificates curl gnupg lsb-release
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -10,10 +10,9 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
 ## Code to deploy Juice Shop
-# docker pull bkimminich/juice-shop
-# docker run -d -p 80:3000 bkimminich/juice-shop
+docker pull bkimminich/juice-shop
+docker run -d -p 80:3000 bkimminich/juice-shop
 
 ## Code to deploy Hello World 
 # docker pull crccheck/hello-world
