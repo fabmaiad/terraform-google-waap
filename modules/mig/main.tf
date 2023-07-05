50a65b7cdd5572d3e170bb3cdeabe7dbe26256ae
Matheus Buitrago <mbuitrago@ciandt.com>
2023-07-05 12:29:48 -0300

    fix: lint errors

--- modules/mig/main.tf
+++ modules/mig/main.tf
@@ -90,21 +90,6 @@ resource "google_compute_instance_group_manager" "mig" {
     # min_ready_sec                  = 50
     # replacement_method             = "RECREATE"
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  update_policy {
-    type                           = "PROACTIVE"
-    minimal_action                 = "REPLACE"
-    most_disruptive_allowed_action = "REPLACE"
-    max_surge_fixed                = 2
-    # max_unavailable_fixed          = 2
-    # min_ready_sec                  = 50
-    # replacement_method             = "RECREATE"
-  }
-
   lifecycle {
     create_before_destroy = true
   }
